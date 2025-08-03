7245a2f2e28db45170a60956efe786619c5b38f7
Totto16 <tobiausgais@gmail.com>
2025-08-03 03:44:27 +0200

    wasm: first draft

---
+++ src/wasm/assert.h
@@ -0,0 +1,9 @@
+
+
+#pragma once
+
+
+#include "./platform.h"
+
+
+
---
+++ src/wasm/platform.h
@@ -0,0 +1,7 @@
+
+
+#pragma once
+
+// js needs to provide these things, when using the resulting wasm file
+
+extern void platform_assert(bool value, const char* message);
