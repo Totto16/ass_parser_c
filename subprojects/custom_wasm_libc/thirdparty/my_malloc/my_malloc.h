c106112399d27b967dddb698b6e659eac713cd40
Totto16 <tobiausgais@gmail.com>
2025-08-04 22:50:03 +0200

    add original my_alloc implementation

---
+++ subprojects/custom_wasm_libc/thirdparty/my_malloc/my_malloc.h
@@ -0,0 +1,23 @@
+// header include guard
+#ifndef _MY_MALLOC_H_
+#define _MY_MALLOC_H_
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void* my_malloc(uint64_t size);
+void my_free(void* ptr);
+void* my_realloc(void* ptr, uint64_t size);
+
+void my_allocator_init(uint64_t size, bool force_alloc);
+void my_allocator_destroy(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
