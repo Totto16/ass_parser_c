68d76fcb90422e1e9790afc9c44fde32795884b9
Totto16 <tobiausgais@gmail.com>
2025-08-05 15:36:04 +0200

    feat: refactor wasm into enclosed class

--- src/wasm/js/wasm.ts
+++ src/wasm/js/wasm.ts
@@ -39,6 +39,10 @@ export interface ParseSettingsJS {
 	todo: number
 }
 
+export interface AssParseResultJS {
+	todo: number
+}
+
 interface AllocatorStatisticsImpl<A> {
 	total: A
 	free: A
@@ -81,177 +85,196 @@ interface WASM extends WebAssembly.WebAssemblyInstantiatedSource {
 	instance: WASMInstance
 }
 
-let wasm: null | WASM = null
+export class WasmBinding {
+	wasm: WASM
 
-function get_memory_buffer(): MemBuf {
-	if (!wasm) {
-		throw new Error("Wasm is not initialized")
+	private constructor(wasm: WASM) {
+		this.wasm = wasm
 	}
 
-	return wasm.instance.exports.memory.buffer
-}
-
-function get_allocator(): Allocator {
-	if (!wasm) {
-		throw new Error("Wasm is not initialized")
+	private get_memory_buffer(): MemBuf {
+		return this.wasm.instance.exports.memory.buffer
 	}
 
-	return wasm.instance.exports
-}
+	private get_allocator(): Allocator {
+		return this.wasm.instance.exports
+	}
 
-// void platform_panic(const char* file_path, int line, const char* message);
-function platform_panic(
-	file_path_ptr: Ptr<Char>,
-	line: Int,
-	message_ptr: Ptr<Char>
-): void {
-	const buffer = get_memory_buffer()
-	const file_path = cstr_by_ptr(buffer, file_path_ptr)
-	const message = cstr_by_ptr(buffer, message_ptr)
-	console.error(file_path + ":" + get_int(line) + ": " + message)
-	// TODO: WASM platform_panic() does not halt the game
-}
-// void platform_log(const char* message);
-function platform_log(message_ptr: Ptr<Char>): void {
-	const buffer = get_memory_buffer()
-	const message = cstr_by_ptr(buffer, message_ptr)
-	console.log(message)
-}
+	// void platform_panic(const char* file_path, int line, const char* message);
+	private platform_panic(
+		file_path_ptr: Ptr<Char>,
+		line: Int,
+		message_ptr: Ptr<Char>
+	): void {
+		const buffer = this.get_memory_buffer()
+		const file_path = cstr_by_ptr(buffer, file_path_ptr)
+		const message = cstr_by_ptr(buffer, message_ptr)
+		console.error(file_path + ":" + get_int(line) + ": " + message)
+		// TODO: WASM platform_panic() does not halt the game
+	}
+	// void platform_log(const char* message);
+	private platform_log(message_ptr: Ptr<Char>): void {
+		const buffer = this.get_memory_buffer()
+		const message = cstr_by_ptr(buffer, message_ptr)
+		console.log(message)
+	}
 
-// void platform_error(const char* message);
-function platform_error(message_ptr: Ptr<Char>): void {
-	const buffer = get_memory_buffer()
-	const message = cstr_by_ptr(buffer, message_ptr)
-	console.error(message)
-}
+	// void platform_error(const char* message);
+	private platform_error(message_ptr: Ptr<Char>): void {
+		const buffer = this.get_memory_buffer()
+		const message = cstr_by_ptr(buffer, message_ptr)
+		console.error(message)
+	}
 
-// void platform_string_conversion(void* data, size_t len, const char* format, void** out_data, size_t* out_len);
-function platform_string_conversion(
-	data_ptr: Ptr<Void>,
-	len: SizeT,
-	format_ptr: Ptr<Char>,
-	out_data_ptr: Ptr<Ptr<Void>>,
-	out_len_ptr: Ptr<SizeT>
-): void {
-	const buffer = get_memory_buffer()
-	const allocator = get_allocator()
+	// void platform_string_conversion(void* data, size_t len, const char* format, void** out_data, size_t* out_len);
+	private platform_string_conversion(
+		data_ptr: Ptr<Void>,
+		len: SizeT,
+		format_ptr: Ptr<Char>,
+		out_data_ptr: Ptr<Ptr<Void>>,
+		out_len_ptr: Ptr<SizeT>
+	): void {
+		const buffer = this.get_memory_buffer()
+		const allocator = this.get_allocator()
 
-	const format = cstr_by_ptr(buffer, format_ptr)
+		const format = cstr_by_ptr(buffer, format_ptr)
 
-	try {
-		let decoder: TextDecoder
+		try {
+			let decoder: TextDecoder
 
-		// see https://developer.mozilla.org/de/docs/Web/API/Encoding_API/Encodings
-		switch (format) {
-			case "UTF-16BE": {
-				decoder = new TextDecoder("utf-16be")
-				break
+			// see https://developer.mozilla.org/de/docs/Web/API/Encoding_API/Encodings
+			switch (format) {
+				case "UTF-16BE": {
+					decoder = new TextDecoder("utf-16be")
+					break
+				}
+				case "UTF-16LE": {
+					decoder = new TextDecoder("utf-16le")
+					break
+				}
+				case "UTF-32BE": {
+					throw new Error("utf-32 be encoding not yet supported")
+					break
+				}
+				case "UTF-32LE": {
+					throw new Error("utf-32 le encoding not yet supported")
+					break
+				}
+				default: {
+					throw new Error(`unrecognized format: ${format}`)
+				}
 			}
-			case "UTF-16LE": {
-				decoder = new TextDecoder("utf-16le")
-				break
-			}
-			case "UTF-32BE": {
-				throw new Error("utf-32 be encoding not yet supported")
-				break
-			}
-			case "UTF-32LE": {
-				throw new Error("utf-32 le encoding not yet supported")
-				break
-			}
-			default: {
-				throw new Error(`unrecognized format: ${format}`)
-			}
+
+			const data: ArrayBufferView = get_sized_ptr_from_memory(buffer, {
+				data_ptr,
+				len,
+			})
+
+			const string = decoder.decode(data)
+
+			const allocated_string = allocate_js_utf8_string(
+				buffer,
+				allocator,
+				string
+			)
+
+			write_ptr_to_memory(buffer, out_data_ptr, allocated_string.data_ptr)
+
+			write_size_t_to_memory(buffer, out_len_ptr, allocated_string.len)
+		} catch (err) {
+			const error_ptr = construct_ptr_error(
+				buffer,
+				allocator,
+				(err as Error).message
+			)
+
+			write_ptr_to_memory(buffer, out_data_ptr, error_ptr.data_ptr)
+
+			write_size_t_to_memory(buffer, out_len_ptr, error_ptr.len)
+
+			return
 		}
-
-		const data: ArrayBufferView = get_sized_ptr_from_memory(buffer, {
-			data_ptr,
-			len,
-		})
-
-		const string = decoder.decode(data)
-
-		const allocated_string = allocate_js_utf8_string(
-			buffer,
-			allocator,
-			string
-		)
-
-		write_ptr_to_memory(buffer, out_data_ptr, allocated_string.data_ptr)
-
-		write_size_t_to_memory(buffer, out_len_ptr, allocated_string.len)
-	} catch (err) {
-		const error_ptr = construct_ptr_error(
-			buffer,
-			allocator,
-			(err as Error).message
-		)
-
-		write_ptr_to_memory(buffer, out_data_ptr, error_ptr.data_ptr)
-
-		write_size_t_to_memory(buffer, out_len_ptr, error_ptr.len)
-
-		return
-	}
-}
-
-export async function startWasm() {
-	const numPages = 4
-	const memory = new WebAssembly.Memory({ initial: numPages })
-
-	const result: WASM = (await WebAssembly.instantiateStreaming(
-		fetch("./static/ass_parser.wasm"),
-		{
-			env: {
-				platform_panic,
-				platform_log,
-				platform_error,
-				platform_string_conversion,
-				memory,
-			},
-		}
-	)) as WASM
-
-	wasm = result
-}
-
-export function allocator_get_statistics(): AllocatorStatisticsJS {
-	if (!wasm) {
-		throw new Error("Wasm is not initialized")
 	}
 
-	const c_statictics: WasmStructRef<AllocatorStatistics> =
-		wasm.instance.exports.allocator_get_statistics()
+	public static async getInstance(path: string): Promise<WasmBinding> {
+		const numPages = 4
+		const memory = new WebAssembly.Memory({ initial: numPages })
 
-	const result: AllocatorStatisticsImpl<UInt64T> = {
-		free: wasm.instance.exports.allocator_statistics_get_free(c_statictics),
-		total: wasm.instance.exports.allocator_statistics_get_total(
-			c_statictics
-		),
-		used: wasm.instance.exports.allocator_statistics_get_used(c_statictics),
-		metadata:
-			wasm.instance.exports.allocator_statistics_get_metadata(
+		const result: WASM = (await WebAssembly.instantiateStreaming(
+			fetch("./static/ass_parser.wasm"),
+			{
+				env: {
+					platform_panic: () => {
+						wasm.platform_panic.bind(wasm)
+					},
+					platform_log: () => {
+						wasm.platform_log.bind(wasm)
+					},
+					platform_error: () => {
+						wasm.platform_error.bind(wasm)
+					},
+					platform_string_conversion: () => {
+						wasm.platform_string_conversion.bind(wasm)
+					},
+					memory,
+				},
+			}
+		)) as WASM
+
+		const wasm = new WasmBinding(result)
+
+		return wasm
+	}
+
+	public allocator_get_statistics(): AllocatorStatisticsJS {
+		const c_statictics: WasmStructRef<AllocatorStatistics> =
+			this.wasm.instance.exports.allocator_get_statistics()
+
+		const result: AllocatorStatisticsImpl<UInt64T> = {
+			free: this.wasm.instance.exports.allocator_statistics_get_free(
 				c_statictics
 			),
+			total: this.wasm.instance.exports.allocator_statistics_get_total(
+				c_statictics
+			),
+			used: this.wasm.instance.exports.allocator_statistics_get_used(
+				c_statictics
+			),
+			metadata:
+				this.wasm.instance.exports.allocator_statistics_get_metadata(
+					c_statictics
+				),
+		}
+
+		const js_result: AllocatorStatisticsJS = {
+			free: get_uint64_t(result.free),
+			total: get_uint64_t(result.total),
+			used: get_uint64_t(result.used),
+			metadata: get_uint64_t(result.metadata),
+		}
+
+		return js_result
 	}
 
-	const js_result: AllocatorStatisticsJS = {
-		free: get_uint64_t(result.free),
-		total: get_uint64_t(result.total),
-		used: get_uint64_t(result.used),
-		metadata: get_uint64_t(result.metadata),
+	public parse_ass(
+		source: string,
+		settings: ParseSettingsJS
+	): AssParseResultJS {
+		const ass_source: WasmStructRef<AssSource> =
+			this.wasm.instance.exports.source_from_string(source)
+
+		const parse_settings: WasmStructRef<ParseSettings> =
+			this.wasm.instance.exports.settings_from_js(settings)
+
+		const result = this.wasm.instance.exports.parse_ass(
+			ass_source,
+			parse_settings
+		)
+
+		//TODO
+
+		return {
+			todo: 0,
+		}
 	}
-
-	return js_result
-}
-
-export function parse_ass(source: string): { todo: number } {
-	if (!wasm) {
-		throw new Error("Wasm is not initialized")
-	}
-
-	const ass_source: AssSource =
-		wasm.instance.exports.source_from_string(source)
-
-	const result = wasm.instance.exports.parse_ass(ass_source, settings)
 }
