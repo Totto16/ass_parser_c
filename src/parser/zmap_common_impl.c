9bf48a63113a4b59f584e43349b09cf4f74d570f
Totto16 <tobiausgais@gmail.com>
2025-11-29 21:16:25 +0100

    ifx: implement missing functions - this are the global hash and compare functions for the keys

--- src/parser/zmap_common_impl.c
+++ src/parser/zmap_common_impl.c
@@ -7,3 +7,11 @@
 ZMAP_IMPLEMENT_MAP_TYPE(char*, CHAR_PTR_KEYNAME, FinalStr, SectionFieldEntry)
 
 ZMAP_IMPLEMENT_MAP_TYPE(char*, CHAR_PTR_KEYNAME, ExtraSectionEntry, ExtraSectionHashMapEntry)
+
+ZMAP_HASH_FUNC_SIG(char*, CHAR_PTR_KEYNAME) {
+	return ZMAP_HASH_STR(key);
+}
+
+ZMAP_COMPARE_FUNC_SIG(char*, CHAR_PTR_KEYNAME) {
+	return strcmp(key1, key2);
+}
